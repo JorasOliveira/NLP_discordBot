b68d620f845d8397e700f82ac5ecb9aa2b9501ee
JorasOliveira <joras.custodio@gmail.com>
2023-02-14 11:50:22 -0300

    chatbot almost done

---
+++ bot.py
@@ -0,0 +1,55 @@
+import dcToken
+import discord
+
+#print(dcToken.apiToken)
+
+#client = discord.Client()
+
+intents = discord.Intents.default()
+intents.members = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    guild = discord.utils.get(client.guilds, name='A Cidade dos Robôs')
+    channel = discord.utils.get(guild.text_channels, name='bot-fest')
+    await channel.send('O bot está online!')
+
+
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.lower() == '!oi':
+        await message.channel.send('Olá!')
+
+    if message.content.lower() == '!source':
+            await message.channel.send('Meu codigo fonte esta em: https://github.com/JorasOliveira/NLP_discordBot')
+
+    if message.content.lower() == '!author':
+            await message.channel.send('Meu autor eh o Jorás, o email dele eh: jorascco@al.insper.edu.br')
+
+    #TODO - fazer isso funcionar direito
+    if message.content.lower() == '!shrek':
+            await message.channel.send('⢀⡴⠑⡄⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣤⣤⣀')
+            await message.channel.send('⠸⡇⠀⠿⡀⠀⠀⠀⣀⡴⢿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀')
+            await message.channel.send('    ⠑⢄⣠⠾⠁⣀⣄⡈⠙⣿⣿⣿⣿⣿⣿⣿⣿⣆             ')
+            await message.channel.send('        ⠁⠀⠀⠈⠙⠛⠂⠈⣿⣿⣿⣿⣿⠿⡿⢿⣆            ')
+            await message.channel.send('     ⢀⡾⣁⣀⠀⠴⠂⠙⣗⡀⠀⢻⣿⣿⠭⢤⣴⣦⣤⣹⠀⠀⠀⢀⢴⣶⣆')
+            await message.channel.send('    ⢀⣾⣿⣿⣿⣷⣮⣽⣾⣿⣥⣴⣿⣿⡿⢂⠔⢚⡿⢿⣿⣦⣴⣾⠁⠸⣼⡿ ')
+            await message.channel.send('  ⢀⡞⠁⠙⠻⠿⠟⠉⠀⠛⢹⣿⣿⣿⣿⣿⣌⢤⣼⣿⣾⣿⡟⠉⠀⠀⠀⠀⠀  ')
+            await message.channel.send('    ⣾⣷⣶⠇⠀⠀⣤⣄⣀⡀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ ')
+            await message.channel.send('    ⠉⠈⠉⠀⠀⢦⡈⢻⣿⣿⣿⣶⣶⣶⣶⣤⣽⡹⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ ')
+            await message.channel.send('           ⠉⠲⣽⡻⢿⣿⣿⣿⣿⣿⣿⣷⣜⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀') 
+            await message.channel.send('           ⢸⣿⣿⣷⣶⣮⣭⣽⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀ ')
+            await message.channel.send('          ⣈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀ ')
+            await message.channel.send('       ⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀')
+            await message.channel.send('        ⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀') 
+            await message.channel.send('          ⠉⠛⠻⠿⠿⠿⠿⠛⠉              ')
+
+client.run(dcToken.apiToken)
